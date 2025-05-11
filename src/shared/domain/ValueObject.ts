252c49acb3662d7e1f0f5a4f0c7da5ce55af9ad5
Asher Novelli <asherndebortoli@gmail.com>
2025-05-11 18:09:28 -0300

    fix:fix bug in ValueObject class

--- src/shared/domain/ValueObject.ts
+++ src/shared/domain/ValueObject.ts
@@ -2,8 +2,11 @@ import { isEqual } from "lodash";
 
 export abstract class ValueObject {
   public equals(vo: this): boolean {
-    if (vo === this || vo === undefined) {
-      return false;
+    if(vo === this) {
+      return true
+    }
+    if(vo === null || vo === undefined) {
+      return false
     }
 
     if (vo.constructor.name !== this.constructor.name) {

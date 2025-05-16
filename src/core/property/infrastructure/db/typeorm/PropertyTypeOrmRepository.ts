21692c793979b3ff2c5f2c9a8ccae5ff3253bcf0
Victor Luís <victorluiscaguiar@gmail.com>
2025-05-16 17:52:27 -0300

    feat: add findByProducerId method and implement findById

--- src/core/property/infrastructure/db/typeorm/PropertyTypeOrmRepository.ts
+++ src/core/property/infrastructure/db/typeorm/PropertyTypeOrmRepository.ts
@@ -3,12 +3,22 @@ import { PropertyEntity } from './PropertyEntity';
 import { Property, PropertyId } from 'src/core/property/domain/Property';
 import { PropertyEntityTypeMapper } from './PropertyEntityTypeMapper';
 import { IPropertyRepository } from 'src/core/property/domain/IPropertyRepository';
+import { ProducerId } from 'src/core/producer/domain/Producer';
 
 export class PropertyTypeOrmRepository implements IPropertyRepository {
   private ormRepository: Repository<PropertyEntity>;
 
   constructor(ormRepository: Repository<PropertyEntity>) {
     this.ormRepository = ormRepository;
+  }
+
+  async findByProducerId(producerId: ProducerId): Promise<Property[]> {
+    const properties = await this.ormRepository.findBy({
+      producerId: producerId.id,
+    });
+    return properties.map((property) =>
+      PropertyEntityTypeMapper.toDomain(property),
+    );
   }
 
   async insert(entity: Property): Promise<void> {
@@ -40,7 +50,10 @@ export class PropertyTypeOrmRepository implements IPropertyRepository {
     throw new Error('Method not implemented.');
   }
 
-  findById(entity_id: PropertyId): Promise<Property> {
-    throw new Error('Method not implemented.');
+  async findById(entity_id: PropertyId): Promise<Property> {
+    const property = await this.ormRepository.findOneBy({
+      propertyId: entity_id.id,
+    });
+    return PropertyEntityTypeMapper.toDomain(property);
   }
 }
